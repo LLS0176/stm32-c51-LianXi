9caaf270b6fa5c8fa49999841ac3b95b82251317
LLS0176 <120024318+LLS0176@users.noreply.github.com>
2022-12-22 20:15:18 +0800

    主要代码

--- C51/C51-7/C51-N7/main.c
+++ C51/C51-7/C51-N7/main.c
@@ -1,7 +1,7 @@
-/*#include <reg51.h>
+#include <reg51.h>
 void delay1s(); //1s
-void disp1();   //数字
-void main()     //主函数
+void disp1();   //脢媒脳脰
+void main()     //脰梅潞炉脢媒
 {
     while(1)
 	{
@@ -9,7 +9,7 @@ void main()     //主函数
 	}
 }
 
-void disp1()			//显示
+void disp1()			//脧脭脢戮
 {
     unsigned char led[]={0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f};
 	                    // 0 ,  1 , 2  ,  3 ,  4 ,  5 ,  6 ,  7 ,  8 ,  9
@@ -42,36 +42,3 @@ void delay1s()
 	}
 }
 
-  */
-  #include <reg51.h>
-#define uchar unsigned char
-#define uint unsigned int
-
-sbit P2_0 = P2^0;
-sbit P3_0 = P3^0;
-
-uchar code table[] = {0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f};
-
-void delay(uint z)
-{
-	uint x, y;
-	for(x = z; x > 0; x--)
-		for(y = 110; y > 0; y--);
-}
-
-void main()
-{
-	uchar i;
-	while(1)
-	{
-		for(i = 0; i <= 100; i++)
-		{
-			if(i % 7 == 0 || i % 10 == 7)
-			{
-				P2 = table[i / 10];
-				P3 = table[i % 10];
-				delay(1000);
-			}
-		}
-	}
-}
